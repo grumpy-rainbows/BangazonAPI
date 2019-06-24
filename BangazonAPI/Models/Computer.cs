4df6065cdc8a2d568f067c1ab6d2304c7a1d4b0c
AliAbdulle <alidhobey@gmail.com>
2019-06-24 13:38:05 -0500

    Saved the change

--- BangazonAPI/Models/Computer.cs
+++ BangazonAPI/Models/Computer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace BangazonAPI.Models
 {
     public class Computer
     {
+        public int id { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        [Required]
+        public DateTime DecomissionDate { get; set; }
+
+        public string Make { get; set; }
+
+        [Required]
+        public string Manufacturer { get; set; }
+
     }
 }
--- BangazonAPI/Models/Employee.cs
+++ BangazonAPI/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,20 @@ namespace BangazonAPI.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int IsSuperVisor { get; set; }
+
+        [Required]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
     }
 }
