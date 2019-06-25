92c73220d9b0fd8ba4eb1495a363eb408aa7763d
Jameka Echols <jameka.echols@gmail.com>
2019-06-25 13:54:54 -0500

    able to post correctly, also added the appropiate objects for customer and producttype

--- BangazonAPI/Controllers/ProductController.cs
+++ BangazonAPI/Controllers/ProductController.cs
@@ -44,27 +44,35 @@ namespace BangazonAPI.Controllers
 
         public async Task<IActionResult> GetAllProducts()
         {
-            using(SqlConnection conn = Connection)
+            using (SqlConnection conn = Connection)
             {
                 // step 2 open the connection 
                 conn.Open();
-                using(SqlCommand cmd = conn.CreateCommand())
+                using (SqlCommand cmd = conn.CreateCommand())
                 {
                     // step 3 create and run the query
-                    cmd.CommandText = @"SELECT Id,
-                                                ProductTypeId,
-                                                CustomerId,
-                                                Price,
-                                                Title,
-                                                Description,
-                                                Quantity
-                                        FROM Product;";
+                    cmd.CommandText = $@"SELECT p.Id,
+                                                p.ProductTypeId,
+                                                p.CustomerId,
+                                                p.Price,
+                                                p.Title,
+                                                p.Description,
+                                                p.Quantity,
+                                                pt.Name,
+                                                c.Id CustomerId,
+                                                c.FirstName,
+                                                c.LastName,
+                                                pt.Id ProductTypeId,
+                                                pt.Name ProductTypeName
+                                        FROM Product p
+                                        JOIN Customer c ON c.Id = p.CustomerId
+                                        JOIN ProductType pt ON pt.Id = p.ProductTypeId";
 
                     SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
                     // create a list to hold all the products
                     List<Product> products = new List<Product>();
-                                   
+
                     while (reader.Read())
                     {
 
@@ -78,7 +86,18 @@ namespace BangazonAPI.Controllers
                             Price = reader.GetDecimal(reader.GetOrdinal("Price")),
                             Title = reader.GetString(reader.GetOrdinal("Title")),
                             Description = reader.GetString(reader.GetOrdinal("Description")),
-                            Quantity = reader.GetInt32(reader.GetOrdinal("Quantity"))
+                            Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                            Customer = new Customer
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                            },
+                            ProductType = new ProductType
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
+                                Name = reader.GetString(reader.GetOrdinal("ProductTypeName"))
+                            }
                         };
 
                         // add that new object to the list of products 
@@ -94,7 +113,7 @@ namespace BangazonAPI.Controllers
 
 
         // GET: api/Product/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "GetProduct")]
         public async Task<IActionResult> Get(int id)
         {
             using (SqlConnection conn = Connection)
@@ -102,14 +121,22 @@ namespace BangazonAPI.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = $@"SELECT Id,
-                                                ProductTypeId,
-                                                CustomerId,
-                                                Price,
-                                                Title,
-                                                Description,
-                                                Quantity
-                                        FROM Product
+                    cmd.CommandText = $@"SELECT p.Id,
+                                                p.ProductTypeId,
+                                                p.CustomerId,
+                                                p.Price,
+                                                p.Title,
+                                                p.Description,
+                                                p.Quantity,
+                                                pt.Name,
+                                                c.Id CustomerId,
+                                                c.FirstName,
+                                                c.LastName,
+                                                pt.Id ProductTypeId,
+                                                pt.Name ProductTypeName
+                                        FROM Product p
+                                        JOIN Customer c ON c.Id = p.CustomerId
+                                        JOIN ProductType pt ON pt.Id = p.ProductTypeId
                                         WHERE Id = @id;";
 
                     cmd.Parameters.Add(new SqlParameter("@id", id));
@@ -119,7 +146,7 @@ namespace BangazonAPI.Controllers
                     Product product = null;
                     while (reader.Read())
                     {
-                        if(product == null)
+                        if (product == null)
                         {
                             product = new Product
                             {
@@ -129,13 +156,23 @@ namespace BangazonAPI.Controllers
                                 Price = reader.GetDecimal(reader.GetOrdinal("Price")),
                                 Title = reader.GetString(reader.GetOrdinal("Title")),
                                 Description = reader.GetString(reader.GetOrdinal("Description")),
-                                Quantity = reader.GetInt32(reader.GetOrdinal("Quantity"))
+                                Quantity = reader.GetInt32(reader.GetOrdinal("Quantity")),
+                                Customer = new Customer
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                                },
+                                ProductType = new ProductType
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ProductTypeId")),
+                                    Name = reader.GetString(reader.GetOrdinal("ProductTypeName"))
+                                }
                             };
                         }
                     }
                     // step 5 close the connection and return the single product
                     reader.Close();
-
                     return Ok(product);
                 }
             }
@@ -143,8 +180,30 @@ namespace BangazonAPI.Controllers
 
         // POST: api/Product
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Product product)
         {
+            using (SqlConnection conn = Connection)
+            {
+                // open the connection 
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // create the query and run it 
+                    cmd.CommandText = $@"INSERT INTO Product (ProductTypeId, CustomerId, Price, Title, [Description], Quantity)
+                                         OUTPUT INSERTED.id
+                                         VALUES (@productTypeId, @customerId, @price, @title, @description, @quantity);";
+                    cmd.Parameters.Add(new SqlParameter("@productTypeId", product.ProductTypeId));
+                    cmd.Parameters.Add(new SqlParameter("@customerId", product.CustomerId));
+                    cmd.Parameters.Add(new SqlParameter("@price", product.Price));
+                    cmd.Parameters.Add(new SqlParameter("@title", product.Title));
+                    cmd.Parameters.Add(new SqlParameter("@description", product.Description));
+                    cmd.Parameters.Add(new SqlParameter("@quantity", product.Quantity));
+
+                    product.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+                }
+            }
         }
 
         // PUT: api/Product/5
