4f753d619a9a587a923ee57fa761636958c9bf23
Billy Mathison <wlmathison@gmail.com>
2019-06-25 13:22:53 -0500

    Adding tests for get single payment type and test for non existant payment type to fail

--- TestBangazonAPI/TestPaymentType.cs
+++ TestBangazonAPI/TestPaymentType.cs
@@ -26,5 +26,34 @@ namespace TestBangazonAPI
                 Assert.True(paymentTypes.Count > 0);
             }
         }
+
+        [Fact]
+        public async Task Test_Single_PaymentType()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/PaymentType/2");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentType = JsonConvert.DeserializeObject<PaymentType>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.Equal(12345, paymentType.AcctNumber);
+                Assert.Equal("Visa", paymentType.Name);
+                Assert.NotNull(paymentType);
+            }
+        }
+
+                [Fact]
+        public async Task Test_Get_NonExitant_PaymentType_Fails()
+        {
+
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/paymentType/999999999");
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
     }
 }
