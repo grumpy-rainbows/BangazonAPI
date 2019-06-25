07ca05f4e9466c7ecb3cc1367ddab4b7df835a22
Billy Mathison <wlmathison@gmail.com>
2019-06-25 11:47:16 -0500

    Creating TestPaymentType file in TestBangazonApi. Adding test to get all payment types and verified that test ran successfully.

---
+++ TestBangazonAPI/TestPaymentType.cs
@@ -0,0 +1,30 @@
+﻿using BangazonAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestBangazonAPI
+{
+    public class TestPaymentType
+    {
+        [Fact]
+        public async Task Test_Get_All_PaymentTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/PaymentType");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentTypes = JsonConvert.DeserializeObject<List<PaymentType>>(responseBody);
+
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(paymentTypes.Count > 0);
+            }
+        }
+    }
+}
