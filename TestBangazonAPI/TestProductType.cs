92c68fc71bfb5b0bc2cde6802372d17564304c32
AliAbdulle <alidhobey@gmail.com>
2019-06-26 09:17:42 -0500

    New Delete Test class has createdand finshed

--- TestBangazonAPI/TestProductType.cs
+++ TestBangazonAPI/TestProductType.cs
@@ -116,7 +116,7 @@ namespace TestBangazonAPI
         {
             using (var client = new APIClientProvider().Client)
             {
-                var response = await client.DeleteAsync("/api/productType/14");
+                var response = await client.DeleteAsync("/api/productType/17");
 
                 Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
             }
