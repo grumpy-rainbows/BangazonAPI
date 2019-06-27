5df305066f92ce957727333278538b97ccaa1b8e
AliAbdulle <alidhobey@gmail.com>
2019-06-27 13:38:49 -0500

    Added comment to my code

--- BangazonAPI/Controllers/EmployeeController.cs
+++ BangazonAPI/Controllers/EmployeeController.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿
+/*
+  Author: Ali Abdulle
+                Purpose: Creating a Controller Class
+                Mothod: Get, Post, and Put
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
